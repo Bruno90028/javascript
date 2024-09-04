39cc6ee8bd5a653a0465713d62dc46156f81b1aa
Bruno90028 <bruno.camacedo.2017@gmail.com>
2024-09-04 15:53:02 -0300

    treino

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,20 +1,51 @@
-function verificar() {
+function verificar () {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
 
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('Verifique os dados e tente novamente')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique novamente!')
     } else {
-        var fsex = document.getElementById('radsex')
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'crian-homem.png')
+            } else if (idade < 18) {
+                //adolescente
+                img.setAttribute('src', 'adolec-homem.png')
+            } else if (idade <= 50) {
+                //adulto
+                img.setAttribute('src', 'homemadulto.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso.png')
+            }
+
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'crian-mulher.png')
+            } else if (idade < 18) {
+                //adolescente
+                img.setAttribute('src', 'adolec-mulher.png')
+            } else if (idade <= 50) {
+                //adulto
+                img.setAttribute('src', 'mulheradulta.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosa.png')
+            }
         }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.</p>`
+        res.appendChild(img)
     }
 }
---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+
+/*
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+*/
